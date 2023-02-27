29c5114786efdeb55eac18f19a967761479affb1
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-02-27 23:35:12 +0100

    update git signs config

--- tasks/software/nvim/nvim-config/lua/tobiasz/plugins/gitsigns.lua
+++ tasks/software/nvim/nvim-config/lua/tobiasz/plugins/gitsigns.lua
@@ -3,11 +3,11 @@ return {
   config = function()
     require("gitsigns").setup({
       signs = {
-        add = { text = "│" },
-        change = { text = "│" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
+        add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
         untracked = { text = "┆" },
       },
       signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
@@ -33,7 +33,7 @@ return {
       max_file_length = 40000, -- Disable if file is longer than this (in lines)
       preview_config = {
         -- Options passed to nvim_open_win
-        border = "single",
+        border = "solid",
         style = "minimal",
         relative = "cursor",
         row = 0,
